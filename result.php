0ee8d337ed8ff07e5d61b9a5c86e61635753b9b1
VanshGilhotra <vanshgilhotra00992@gmail.com>
2025-06-26 10:39:40 +0530

    Initial Commit

---
+++ result.php
@@ -0,0 +1,123 @@
+<?php require_once("header.php"); ?>
+
+<!--Start breadcrumb area-->     
+<section class="breadcrumb-area" style="background-image: url(assets/images/breadcrumb-bg.jpg);">
+    <div class="container">
+        <div class="row">
+            <div class="col-xl-12">
+                <div class="inner-content text-center clearfix">
+                    <div class="title wow slideInUp animated" data-wow-delay="0.3s" data-wow-duration="1500ms">
+                       <h2>Our Results</h2>
+                    </div>
+                    <div class="breadcrumb-menu wow slideInDown animated" data-wow-delay="0.3s" data-wow-duration="1500ms">
+                        <ul class="clearfix">
+                            <li><a href="index.php">Home</a></li>
+                            <li><i class="fa fa-angle-double-right" aria-hidden="true"></i></li>
+                            <li class="active">Result</li>
+                        </ul>    
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--End breadcrumb area-->
+
+
+<section class="select-country-style3-area country-page default-padding">
+    <div class="container">
+
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall1.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall1.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall2.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall2.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall3.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall3.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall4.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall4.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall6.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall6.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall7.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall7.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-xl-4 col-lg-4">
+                        <div class="single-select-country-style3">
+                            <div class="img-holder">
+                                <div class="">
+                                    <a class="example-image-link" href="assets/images/gall5.jpg" data-lightbox="example-set">
+                                        <img src="assets/images/gall5.jpg" alt="..." />
+                                    </a>
+                                </div>
+                            </div>    
+                        </div>    
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>    
+</section>
+
+<?php require_once("footer.php"); ?>
